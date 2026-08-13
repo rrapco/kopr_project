9921f3fa9c5577fd59a37779c3333ad2fa645d02
Roman Rapco <114595620+rrapco@users.noreply.github.com>
2026-08-13 10:06:19 +0200

    Update Constants.java

--- file_copy/src/main/java/sk/upjs/kopr/file_copy/Constants.java
+++ file_copy/src/main/java/sk/upjs/kopr/file_copy/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 
 	public static final int SERVER_PORT = 5001;
 	public static final String SERVER_HOST = "localhost";
-	public static final String FROM_DIR = "C:\\Users\\roman\\Desktop\\Z\\";
-	public static final String TO_DIR = "C:\\Users\\roman\\Desktop\\SEM\\";
+	public static final String FROM_DIR = "";
+	public static final String TO_DIR = "";
 	public static final File POISON_PILL = new File("poison.pill");
 	public static final int BUFFER_SIZE = 16384;
 	
